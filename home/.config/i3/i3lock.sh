c9b8c678d7ce9fe023e93a35a72c2980e2ee0cf0
Nikolas Tsiongas <ntsiongas@gmail.com>
2016-10-17 18:08:59 +0200

    Upgraded i3 looks

--- home/.config/i3/i3lock.sh
+++ home/.config/i3/i3lock.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-scrot /tmp/screen_locked.png
-convert /tmp/screen_locked.png -blur 2x2 /tmp/screen_locked2.png
-i3lock -i /tmp/screen_locked2.png
+scrot /tmp/screen.png
+convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+[[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
+i3lock -u -i /tmp/screen.png
+rm /tmp/screen.png
+
